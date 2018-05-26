c6fadfab17fba846d9df84798d99f9b9e8bf6108
ionFreeman <ion.freeman@baml.com>
2018-05-26 16:32:35 -0400

    Added type checking

--- json_sample.py
+++ json_sample.py
@@ -1,31 +1,35 @@
 from flask import Flask, jsonify, request, json, make_response
+from typing import Dict, List, Tuple
 import logging
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# use old timey variable interpolation because PyCharm is going crazy
-logfile = logging.FileHandler("{}.log".format(__name__))
+logfile = logging.FileHandler(f"{__name__}.log")
 logfile.setFormatter(formatter)
 logger.addHandler(logfile)
 logger.setLevel('INFO')
 
 app = Flask(__name__)
 
-contacts = [0, []]
+# JSON does not have Tuples -- contacts is best modeled as a tuple,
+# but it will be a list in JSON
+contacts: Tuple[int, List[Dict[str, str]]] = [0, []]
 
 @app.route('/contactdb', methods=['GET'])
 def get_contacts():
     try:
         sortedcontactlist = contacts[1]
-        sortedcontactlist.sort(key=lambda x:'{} {}'.format(x["lname"], x["fname"]))
-        logger.info('returning {} contacts {}'.format(contacts[0], sortedcontactlist))
-        # I want to return an ordered map, so I'll build the json myself
-        retval = "{0}, [".format(contacts[0])
+        sortedcontactlist.sort(key=lambda x:f'{x["lname"]} {x["fname"]}')
+        # I want to return an sorted list, so I'll build the json myself
+        retval = f'{{"count":"{contacts[0]}", "contacts":['
         if contacts[0] > 0:
-            first = True
+            first: bool = True
             for jsn in [json.dumps(contact) for contact in sortedcontactlist]:
-                retval = '{}\n{}\t{}'.format(retval, '' if first else ',', jsn)
-        retval += "\n]"
+                comma = u'' if first else u','
+                retval = f'{retval} \n {comma}\t{jsn}'
+                first = False
+        retval += "\n]}"
+        logger.info(f'returning {retval}')
         return make_response(retval)
     except Exception as e:
         logger.error(e)
@@ -38,10 +42,16 @@ def add_contact():
     logger.info(request.get_data(as_text = True))
     contact = request.get_json()
     try:
-        # blow up if fname and lname are not received -- FUNCTIONAL LOG MESSAGE
-        logger.info("received {} {}".format(contact['fname'], contact['lname']))
-        contacts[0] += 1
+        logger.info(f"received {contact}")
+        # scan the list for matching lname, fname
         contactlist = contacts[1]
+        for existing in[old for old in contactlist
+                        # blow up if fname and lname are not received
+                        if old["lname"] == contact["lname"]
+                           and old["fname"] == contact["fname"]]:
+            contacts[0] -= 1
+            contactlist.remove(existing)
+        contacts[0] += 1
         contactlist.append(contact)
         contacts[1] = contactlist
         return jsonify(contacts) # return the new unordered dict
